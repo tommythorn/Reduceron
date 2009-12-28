223c45a4b38afef22a0bddfdebb98919531bf338
Jason Reich <jason@cs.york.ac.uk>
2009-12-28 02:55:02 -0800

    Added infix operators, primitive and backtick style
    darcs-hash:20091228105502-25e1a-dcfe3f90016fdd8a8f367b9b7fc39cbc230c780b.gz

--- Flite/Parsec/Parse.hs
+++ Flite/Parsec/Parse.hs
@@ -6,6 +6,7 @@ module Flite.Parsec.Parse where
 	import Control.Monad
 	import Data.Char
 	import Text.ParserCombinators.Parsec hiding (many, option, (<|>))
+	import Text.ParserCombinators.Parsec.Expr
 	import Text.ParserCombinators.Parsec.Language
 	import qualified Text.ParserCombinators.Parsec.Token as T
 	
@@ -15,7 +16,7 @@ module Flite.Parsec.Parse where
 		, identStart		= letter
 		, identLetter		= alphaNum
 		, opStart			= opLetter haskellStyle
-		, opLetter			= oneOf "<=>-+/\\"
+		, opLetter			= oneOf "<=>-+/"
 		, reservedNames		= ["case", "of", "let", "in", "if", "then", "else"]
 		, caseSensitive		= True
 		}
@@ -47,6 +48,22 @@ module Flite.Parsec.Parse where
 	block p = braces (p `sepEndBy` semi) <?> "block"
 	
 	primitives = ["(+)", "(-)", "(==)", "(/=)", "(<=)", "emit", "emitInt"]
+	
+	-- | Build an application out of an infix operation
+	infixApp t x y = App t [x, y]
+	
+	-- | Detect if an identifier if variable or constructor
+	consOrVar n = if isLower (head n) then Var n else Con n
+	
+	-- | Capture \`InfixFunctions\`
+	infixName = Infix ((symbol "`" *> identifier <* symbol "`") >>= return . infixApp . consOrVar ) AssocLeft
+	
+	-- | Build primitive infix operators
+	binary op assoc = Infix (reservedOp op >> return (infixApp (Fun $ "(" ++ op ++ ")"))) assoc
+	
+	opTable = [   [infixName]
+				, [binary "+" AssocLeft, binary "-" AssocLeft]
+				, [binary "==" AssocNone, binary "/=" AssocNone, binary "<=" AssocNone] ]
 	
 	prim :: Parser Id
 	prim = try $ do
@@ -84,7 +101,10 @@ module Flite.Parsec.Parse where
 		<|> pure App <*> (pure Con <*> con) <*> many pat
 	
 	expr :: Parser Exp
-	expr = pure App <*> expr' <*> many expr'
+	expr = buildExpressionParser opTable expr1
+	
+	expr1 :: Parser Exp
+	expr1  = pure App <*> expr' <*> many expr'
 	
 	expr' :: Parser Exp
 	expr' = pure Lam <*> ((symbol "\\" <?> "lambda abstraction") *> many var) <*> (reservedOp "->" *> expr)
