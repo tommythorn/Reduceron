6b89035c8a146d648eb798c0e5917a76cac23e84
Tommy Thorn <tommy-github@thorn.ws>
2011-08-15 22:48:26 -0700

    Make shell scripts executable

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
-find | grep "\.hi$" | xargs rm -vf {}
-find | grep "\.o$" | xargs rm -vf {}
-find | grep "~$" | xargs rm -vf {}
+find . -name "*.hi" | xargs rm -vf {}
+find . -name "*.o" | xargs rm -vf {}
+find . -name "*~" | xargs rm -vf {}
 rm -vf fl
