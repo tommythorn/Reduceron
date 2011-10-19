79534bbdffda8865e7a2e64485d0313fd50d9eaa
Tommy Thorn <tommy-github@thorn.ws>
2011-10-18 23:38:00 -0700

    Reduceron: get Verilog simulations going again, add lpm_add_sub.v

---
+++ york-lava/simulation/lpm_add_sub.v
@@ -0,0 +1,19 @@
+module lpm_add_sub (result, cout, overflow, add_sub, cin, dataa, datab, clock, clken, aclr);
+   parameter lpm_type = "lpm_add_sub";
+   parameter lpm_width = 1;
+   parameter lpm_direction  = "UNUSED";
+   parameter lpm_representation = "UNSIGNED";
+   parameter lpm_pipeline = 0;
+   parameter lpm_hint = "UNUSED";
+   input [lpm_width-1:0] dataa, datab;
+   input                 add_sub, cin;
+   input                 clock;
+   input                 clken;
+   input                 aclr;
+   output [lpm_width-1:0] result;
+   output                 cout, overflow;
+
+//   assign                 {cout,result} = add_sub ? dataa - datab - cin : dataa + datab + cin;
+   assign                 {cout,result} = dataa + datab + cin;
+   assign                 overflow = 'h x;
+endmodule
