874dd2b204dbedff272c475074d1c35b008f50e3
Tommy Thorn <tommy-github@thorn.ws>
2015-01-22 22:04:50 -0800

    Workload Hello with a denser text representation.
    We could be twice as dense if we had shift primities, and four times
    as dense with 32-bit integers (at which point a single template could
    encode a 26-char string).

--- programs/Hello.hs
+++ programs/Hello.hs
@@ -1,7 +1,15 @@
 {
-main = st32 0 'H' (
-       st32 0 'e' (
-       st32 0 'l' (
-       st32 0 'l' (
-       st32 0 'o' 42))));
+text = DSn 'A' 't' (DSn ' ' 'v' (DSn 'e' 'r' (DSn 'o' ' ' (DSn 'e' 'o' (DSn 's' ' ' (DSn 'e' 't' (DSn ' ' 'a' (DSn 'c' 'c' (DSn 'u' 's' (DSn 'a' 'm' (DSn 'u' 's' (DSn ' ' 'e' (DSn 't' ' ' (DSn 'i' 'u' (DSn 's' 't' (DSn 'o' ' ' (DSn 'o' 'd' (DSn 'i' 'o' (DSn ' ' 'd' (DSn 'i' 'g' (DSn 'n' 'i' (DSn 's' 's' (DSn 'i' 'm' (DSn 'o' 's' (DSn ' ' 'd' (DSn 'u' 'c' (DSn 'i' 'm' (DSn 'u' 's' (DSn ' ' 'q' (DSn 'u' 'i' (DSn ' ' 'b' (DSn 'l' 'a' (DSn 'n' 'd' (DSn 'i' 't' (DSn 'i' 'i' (DSn 's' ' ' (DS3 '.' '.' '.')))))))))))))))))))))))))))))))))))));
+
+decode cons nil ds = case ds of {
+     DS0			-> nil;
+     DS1 a			-> cons a nil;
+     DS2 a b			-> cons a (cons b nil);
+     DS3 a b c			-> cons a (cons b (cons c nil));
+     DSn a b ds			-> cons a (cons b (decode cons nil ds));
+};
+
+putChar ch rest = st32 0 ch rest;
+
+main = decode putChar 42 text;
 }
---
+++ programs/HelloEncoding.hs
@@ -0,0 +1,71 @@
+-- Encoding a string as [Char] is very inefficient in Reduceron (2 apps per char).
+-- Instead we can encode N chars in an int and up to three ints in an app.
+-- Here we just use N=1
+
+data DenseString
+     = DS0
+     | DS1 Char
+     | DS2 Char Char
+     | DS3 Char Char Char
+     | DSn Char Char DenseString
+     deriving Show
+
+decode :: DenseString -> String
+decode ds = case ds of
+     DS0			-> []
+     DS1 a			-> [a]
+     DS2 a b			-> [a, b]
+     DS3 a b c			-> [a, b, c]
+     DSn a b ds			-> a : b : decode ds
+
+encode s = case s of
+     []               		-> DS0
+     [a]              		-> DS1 a
+     [a, b]           		-> DS2 a b
+     [a, b, c]        		-> DS3 a b c
+     a : b : s			-> DSn a b (encode s)
+
+main = putStrLn $ show $ encode  "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis ..."
+
+
+-- Here we just use N=2 (and 128 bit chars as Reduceron currently only have 15-bit ints)
+
+type TwoChar = Int
+data DenseString2
+     = DSB0
+     | DSB1 Char
+     | DSB2 Char Char
+     | DSB3 Char Char Char
+     | DSB4 TwoChar TwoChar
+     | DSB5 Char TwoChar TwoChar
+     | DSB6 TwoChar TwoChar TwoChar
+     | DSBn TwoChar TwoChar DenseString2
+     deriving Show
+
+enTwo :: Char -> Char -> Int
+enTwo a b = fromEnum a + 128 * fromEnum b
+encode2 s = case s of
+     []                         -> DSB0
+     [a]                        -> DSB1 a
+     [a, b]                     -> DSB2 a b
+     [a, b, c]                  -> DSB3 a b c
+     [a, b, c, d]               -> DSB4 (enTwo a b) (enTwo c d)
+     [a, b, c, d, e]            -> DSB5 a (enTwo b c) (enTwo d e)
+     [a, b, c, d, e, f]         -> DSB6 (enTwo a b) (enTwo c d) (enTwo e f)
+     a : b : c : d : s          -> DSBn (enTwo a b) (enTwo c d) (encode2 s)
+
+deTwo :: Int -> String -> String
+deTwo ab k = toEnum (ab `mod` 128) : toEnum (ab `div` 128) : k
+decode2 :: DenseString2 -> String
+decode2 ds = case ds of
+     DSB0                        -> []
+     DSB1 a                      -> [a]
+     DSB2 a b                    -> [a, b]
+     DSB3 a b c                  -> [a, b, c]
+     DSB4 ab cd                  -> deTwo ab $ deTwo cd []
+     DSB5 a bc de                -> a : deTwo bc (deTwo de [])
+     DSB6 ab cd de               -> deTwo ab $ deTwo cd $ deTwo de []
+     DSBn ab cd ds               -> deTwo ab $ deTwo cd $ decode2 ds
+
+main2 = putStrLn $ show $
+  encode2 "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis ..."
